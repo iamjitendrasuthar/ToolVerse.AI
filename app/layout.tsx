2ea5a71e922319bcc52f5ccde7dfb38fb1d95b29
jitendra <iamjitendrasuthar24@gmail.com>
2026-04-09 18:18:09 +0530

    revert

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,31 +20,7 @@ export default function RootLayout({
         <Provider>
           <LayoutWrapper>
             {children}
-            <Toaster
-              position="top-center"
-              richColors
-              toastOptions={{
-                style: {
-                  borderRadius: "1rem",
-                  border: "1px solid #e2e8f0",
-                },
-                // Customizing success/error to ensure dark text
-                success: {
-                  style: {
-                    background: "#ecfdf5", // Light emerald bg
-                    color: "#064e3b", // Dark emerald text
-                    borderColor: "#10b981",
-                  },
-                },
-                error: {
-                  style: {
-                    background: "#fef2f2", // Light red bg
-                    color: "#7f1d1d", // Dark red text
-                    borderColor: "#ef4444",
-                  },
-                },
-              }}
-            />{" "}
+            <Toaster position="top-center" richColors />
           </LayoutWrapper>
         </Provider>
       </body>
