4f47f6f9a29da1e56ba8b7b55d3e014eb6a26a4a
jitendra <iamjitendrasuthar24@gmail.com>
2026-04-10 09:08:00 +0530

    Fixed html issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className="min-h-full flex flex-col bg-[#fafcff]">
         <Provider>
           <LayoutWrapper>
