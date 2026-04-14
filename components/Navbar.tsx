8ef4a300610348d749411fc5645cbee146103a47
jitendra <iamjitendrasuthar24@gmail.com>
2026-04-14 12:33:51 +0530

    75vh

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -649,11 +649,10 @@ group-hover/menu:translate-y-0 absolute top-full right-0 mt-3 w-64 rounded-[2rem
               : "opacity-0 -translate-y-8 invisible pointer-events-none"
           }`}
         >
-          <div className="bg-white/95 backdrop-blur-2xl overflow-hidden flex flex-col max-h-[80vh] rounded-2xl">
+          <div className="bg-white/95 backdrop-blur-2xl overflow-hidden flex flex-col max-h-[75vh] rounded-2xl">
             {/* 1. Scrollable Main Content Area */}
             <div className="flex-1 overflow-y-auto custom-scrollbar p-4 flex flex-col gap-6">
               {/* SECTION A: Quick Links (Compact Grid) 
-                  Isse bohot saari vertical space bachegi aur users ko bina scroll kiye sab dikhega 
               */}
               <div>
                 <span className="text-[10px] font-bold uppercase tracking-[0.15em] text-neutral-400 pl-2 mb-3 block">
